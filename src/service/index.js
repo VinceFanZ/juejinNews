ae1a462e6f74f4293de3e298e2615e298009c3d1
vince <vincefanz@gmail.com>
2017-07-21 15:16:26 +0800

    Add api

--- src/service/index.js
+++ src/service/index.js
@@ -1,17 +1,56 @@
 import axios from 'axios'
 
 export default {
-  getInitInfo () {
-    return axios.get('/get_entry_by_rank?src=web&limit=20&type=post&category=all')
+  /**
+   * 热门数据
+   * @param {string} category
+   * @returns {object | *}
+   */
+  getRankData (category) {
+    return axios.get('/v1/get_entry_by_rank', {
+      params: {
+        src: 'web',
+        limit: 20,
+        type: 'post',
+        category: category || 'all',
+      }
+    })
       .then((response) => {
         const data = response.data
-        if (data && data.code === 0) {
-          return Promise.resolve(data.data)
+        if (data && data.s === 1) {
+          return Promise.resolve(data.d)
         }
-        return Promise.reject(data.msg)
+        return Promise.reject(data.m)
       })
       .catch((error) => {
-        const msg = typeof error === 'object' || !error ? '服务器开小差了 0.0  请稍后再看' : error
+        const msg = typeof error === 'object' || !error ? '服务器炸了，请稍后再看 ~|~' : error
+        throw msg
+      })
+  },
+
+  /**
+   * 最新数据
+   * @param {string} category
+   * @returns {object | *}
+   */
+  getTimelineData (category) {
+    return axios.get('/v1/get_entry_by_timeline', {
+      params: {
+        src: 'web',
+        limit: 20,
+        type: 'post',
+        category: category || 'all',
+      }
+    })
+      .then((response) => {
+        const data = response.data
+        if (data && data.s === 1) {
+          return Promise.resolve(data.d)
+        }
+        return Promise.reject(data.m)
+      })
+      .catch((error) => {
+        const msg = typeof error === 'object' || !error ? '服务器炸了，请稍后再看 ~|~' : error
         throw msg
       })
   }
