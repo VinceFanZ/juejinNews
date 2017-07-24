81797e530fcee7651743e5e907893bcbaf516688
vince <vincefanz@gmail.com>
2017-07-24 15:29:12 +0800

    Update

--- src/views/all.js
+++ src/views/all.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import service from '../service'
 import Loading from '../components/Loading'
 import Item from '../components/Item'
 
 class All extends React.PureComponent {
+  static defaultProps = {
+    history: {}
+  }
+  static propTypes = {
+    history: PropTypes.object
+  }
+
+  constructor (props) {
+    super(props)
+    this.onClickToFrontend = this.onClickToFrontend.bind(this)
+  }
+
   state = {
     list: [],
     isLoading: true,
@@ -14,6 +27,10 @@ class All extends React.PureComponent {
   }
 
   componentWillUnmount () {
+  }
+
+  onClickToFrontend () {
+    this.props.history.push('/frontend')
   }
 
   async getData () {
@@ -46,6 +63,7 @@ class All extends React.PureComponent {
   render () {
     return (
       <div>
+        <burron onClick={this.onClickToFrontend}>to frontend</burron>
         {
           this.state.isLoading && <Loading />
         }
